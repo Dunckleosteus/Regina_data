f27fc5b3218a7fa7cf3567759b0ad5e49e5d48fb
George Husband <ghus266@gmail.com>
2022-08-16 13:28:00 +0200

    initial commit

---
+++ text_to_shape.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from shapely.geometry import LineString
+import shapely.wkt
+import geopandas 
+import os 
+import matplotlib.pyplot as plt
+
+
+name = os.path.join("N3495D04_Export.TXT")
+df = pd.read_csv(name, encoding = "iso8859_15", sep = "\t")
+for i in range (0, len(df.columns)):
+    print(f"{str(i)} => {df.columns[i]}")
+#print (df.WGS_LAT_ST)
+df["geometry"] = LineString([[0, 0], [0, 0]])# initialize a default geometry field 
+
+for field in [df.columns[103], df.columns[104], df.columns[105], df.columns[106]]:
+    df[field] = df[field].str.replace(",", ".")
+    df[field] = df[field].astype(float)
+
+for row in range(0, len(df.index)):
+    df.at[row, "geometry"] = LineString([[df.at[row, "WGS_LON_ST"], df.at[row, "WGS_LAT_ST"]], [df.at[row, "WGS_LON_EN"], df.at[row, "WGS_LAT_EN"]]])
+
+points_start = geopandas.GeoDataFrame(df, geometry = df["geometry"])
+points_start.plot()
+plt.show()
